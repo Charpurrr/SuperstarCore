ccafe5ee6f011007c570e50d10065d1ba4134207
Charpurrr <yyoshidevv@gmail.com>
2023-11-05 13:33:04 +0100

    Revert "State machine stuff + 3DBG test"
    This reverts commit 2ee9f6b046c9d87780700a3b9d285df17d0372b6.

--- actors/battle/actors/battle_players/states/moving.gd
+++ actors/battle/actors/battle_players/states/moving.gd
@@ -1,27 +1,27 @@
 class_name MovingState
 extends State
-## Moving toward or from an enemy
-#
-#var tweening : bool
-#
-#
-#func _process(_delta):
-#	match state_coordinator.active_state:
-#		"j_moving_toward": # Moving toward the enemy for a jump attack
-#			pass
-#		"h_moving_toward": # Moving toward the enemy for a hammer attack
-#			if not tweening:
-#				var final_pos = actor.bp_coordinator.enemy_coordinator.final_enemy.hammer_holo_pos
-#
-#				tweening = true
-#				MathUtility.tween_current_pos_to(actor, final_pos, 30, self)
-#
-#
-#func tween_finished(): # Called when a tween is finished interpolating
-#	tweening = false
-#
-#	match state_coordinator.active_state:
-#		"j_moving_toward":
-#			state_coordinator.active_state = "atk_jumping"
-#		"h_moving_toward":
-#			state_coordinator.active_state = "atk_hammering"
+# Moving toward or from an enemy
+
+var tweening : bool
+
+
+func _process(_delta):
+	match state_coordinator.active_state:
+		"j_moving_toward": # Moving toward the enemy for a jump attack
+			pass
+		"h_moving_toward": # Moving toward the enemy for a hammer attack
+			if not tweening:
+				var final_pos = actor.bp_coordinator.enemy_coordinator.final_enemy.hammer_holo_pos
+
+				tweening = true
+				MathUtility.tween_current_pos_to(actor, final_pos, 30, self)
+
+
+func tween_finished(): # Called when a tween is finished interpolating
+	tweening = false
+
+	match state_coordinator.active_state:
+		"j_moving_toward":
+			state_coordinator.active_state = "atk_jumping"
+		"h_moving_toward":
+			state_coordinator.active_state = "atk_hammering"
--- actors/battle/bg_applier.gd
+++
@@ -1,5 +0,0 @@
-extends Sprite2D
-
-
-func _process(delta):
-	texture = %SubViewport.get_texture()
